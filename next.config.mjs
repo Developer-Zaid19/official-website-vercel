43da407b010c871eed29194085e20ec15731167f
Developer-Zaid <zk145430@gmail.com>
2026-06-06 22:20:26 +0530

    alhamdulillah working

--- next.config.mjs
+++ next.config.mjs
@@ -4,11 +4,11 @@ const nextConfig = {
     return [
       {
         source: "/notes/pdf/:id",
-        destination: `http://10.85.227.246:5000/note-pdf/${":id"}.pdf`,
+        destination: `https://devzaidbackend.onrender.com/note-pdf/${":id"}.pdf`,
       },
       {
         source: "/notes/img/:id",
-        destination: `http://10.85.227.246:5000/notes-img/${":id"}`,
+        destination: `https://devzaidbackend.onrender.com/notes-img/${":id"}`,
       },
     ];
   },
